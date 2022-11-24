30efa058db5d0432cd584a3251a01a1c89e15b2f
Appazov <anvar978@yandex.ru>
2022-11-24 20:09:33 +0500

    Add search to Main page

--- src/modules/main/pages/Main/Main.js
+++ src/modules/main/pages/Main/Main.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 import { MainLayout } from 'shared'
 import api from '../../config/api'
@@ -9,20 +9,48 @@ import s from './Main.module.scss'
 const Main = () => {
   const [products, setProducts] = useState([])
   const [isLoading, setIsLoading] = useState(false)
+  const [searchInput, setSearchInput] = useState('')
+  const [foundProducts, setFoundProducts] = useState([])
+
+  const searchButtonRef = useRef(null)
 
   useEffect(() => {
     setIsLoading(true)
     api.fetchProducts().then((data) => {
       setProducts(data)
+      setFoundProducts(data)
       setIsLoading(false)
     })
   }, [])
 
+  const onSearch = () => {
+    setFoundProducts(
+      products.filter((product) =>
+        product.title.toLowerCase().includes(searchInput.toLowerCase().trim())
+      )
+    )
+  }
+
   return (
     <MainLayout>
       <div className={s.root}>
+        <div className={s.search}>
+          <input
+            type='text'
+            value={searchInput}
+            onChange={(event) => setSearchInput(event.target.value)}
+            onKeyDown={(event) => {
+              if (event.key === 'Enter') {
+                searchButtonRef.current.click()
+              }
+            }}
+          />
+          <button type={'button'} onClick={onSearch} ref={searchButtonRef}>
+            Поиск
+          </button>
+        </div>
         {!isLoading ? (
-          products.map((product) => (
+          foundProducts.map((product) => (
             <ProductItem
               key={product.id}
               id={product.id}
