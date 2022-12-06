48d98fec1b214240998b5f13706abdc355449dcc
Appazov <anvar978@yandex.ru>
2022-12-06 20:15:12 +0500

    Add auth

--- src/modules/account/pages/Registration/Registration.js
+++ src/modules/account/pages/Registration/Registration.js
@@ -1,7 +1,66 @@
-import React from 'react'
+import { Button, Stack, TextField } from '@mui/material'
+import { useForm } from 'react-hook-form'
+import { MainLayout } from '../../../../shared'
+import * as yup from 'yup'
+import { yupResolver } from '@hookform/resolvers/yup'
+import api from '../../../main/config/api'
+
+const schema = yup
+  .object({
+    username: yup.string().required('Введите логин'),
+    email: yup.string().email('Некорректный email'),
+    password: yup
+      .string()
+      .required('Введите пароль')
+      .max(20, 'Не более 20 символов')
+      .min(8, 'Не менее 8 символов'),
+  })
+  .required()
 
 const Registration = () => {
-  return <div>Registration</div>
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ resolver: yupResolver(schema) })
+
+  const onSubmit = async (formData) => {
+    const data = await api.registration(formData)
+
+    console.log(data)
+  }
+
+  return (
+    <MainLayout>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Stack spacing={2}>
+          <TextField
+            label={'Username'}
+            error={Boolean(errors?.username)}
+            helperText={errors?.username?.message}
+            {...register('username')}
+          />
+          <TextField
+            label={'Email'}
+            error={Boolean(errors?.email)}
+            helperText={errors?.email?.message}
+            {...register('email')}
+          />
+          <TextField
+            label={'Password'}
+            type={'password'}
+            error={Boolean(errors?.password)}
+            helperText={errors?.password?.message}
+            type={'password'}
+            {...register('password')}
+          />
+          <Button type={'submit'} variant={'contained'} size={'large'}>
+            Зарегистрироваться
+          </Button>
+        </Stack>
+      </form>
+    </MainLayout>
+  )
 }
 
 export default Registration
--- src/modules/main/config/api.js
+++ src/modules/main/config/api.js
@@ -17,6 +17,46 @@ const fetchProduct = async (productId) => {
   }
 }
 
-const api = { fetchProducts, fetchProduct }
+const registration = async (formData) => {
+  try {
+    const response = await axios.post('http://localhost:5000/auth/registration', formData)
+    if (response.data) {
+      localStorage.setItem('accessToken', response.data.access_token)
+    }
+    return response.data
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+const login = async (formData) => {
+  try {
+    const response = await axios.post('http://localhost:5000/auth/login', formData)
+    if (response.data) {
+      localStorage.setItem('accessToken', response.data.access_token)
+    }
+    return response.data
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+const getRoles = async () => {
+  try {
+    const accessToken = localStorage.getItem('accessToken')
+    if (accessToken) {
+      const response = await axios.get('http://localhost:5000/roles', {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      })
+      return response.data
+    }
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+const api = { fetchProducts, fetchProduct, registration, login, getRoles }
 
 export default api
